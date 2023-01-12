a5f02c73d61b44d12f8ef2aa40da8b0d360a9e54
alexw <alex.j.witkowski@gmail.com>
2023-01-12 12:10:17 -0500

    changed a litlte

--- src/main/java/com/incorrectway/themod/TheMod.java
+++ src/main/java/com/incorrectway/themod/TheMod.java
@@ -3,8 +3,18 @@ package com.incorrectway.themod;
 import net.fabricmc.api.ModInitializer;
 
 public class TheMod implements ModInitializer {
+    /**
+     * Fabric's main method / main entry point
+     */
     @Override
     public void onInitialize() {
+
+    }
+
+    /**
+     * Our janky entry-point for non-fabric things
+     */
+    public static void main(String[] args) {
 
     }
 }
