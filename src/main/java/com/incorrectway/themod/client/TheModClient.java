7130f11dbf066812b43099baa8b3ee5e4f491005
alexw <alex.j.witkowski@gmail.com>
2023-01-12 10:27:19 -0500

    Base files

---
+++ src/main/java/com/incorrectway/themod/client/TheModClient.java
@@ -0,0 +1,13 @@
+package com.incorrectway.themod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class TheModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
